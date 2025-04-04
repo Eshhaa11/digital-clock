0e5001d6e78ec8f2b47dde668364587761149a23
Esha Patel <Patellesha11@gmail.com>
2025-04-04 21:40:37 +0300

    added useEffect

--- src/Components/clock.js
+++ src/Components/clock.js
@@ -4,7 +4,12 @@ import './Clock.css'
 function Clock () {
     const [clock, setClock] = useState("00:00:00")
      return (
-        useEffect(() => )
+        useEffect(() => {
+         const interval = setInterval(() => {
+            const
+            
+         }, interval);
+        })
         <h1>DIGITAL CLOCK</h1>
      )
 }
