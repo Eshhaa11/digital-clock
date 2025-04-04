7f00fa9936f692dea68a3c0be1a94eff463ea3c3
Esha Patel <Patellesha11@gmail.com>
2025-04-04 21:45:57 +0300

    added

--- src/Components/clock.js
+++ src/Components/clock.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-function Clock() { 
+function Clock() {
   const [time, setTime] = useState("00:00:00");
 
-  useEffect(() => { 
-    const interval = setInterval(() => { 
-      const now = new Date(); 
-      setTime(now.toLocaleTimeString()); 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      setTime(now.toLocaleTimeString());
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []); 
+  }, []);
 
-  return <h1>{time}</h1>; 
+  return <h1>{time}</h1>;
 }
 
 export default Clock;
