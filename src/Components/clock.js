0b3b0965cbceae9eebb64768f3cee8ac7b9fc137
Esha Patel <Patellesha11@gmail.com>
2025-04-04 21:43:20 +0300

    added return

--- src/Components/clock.js
+++ src/Components/clock.js
@@ -3,16 +3,16 @@ import './Clock.css'
 
 function Clock () {
     const [clock, setClock] = useState("00:00:00")
-     return (
         useEffect(() => {
          const interval = setInterval(() => {
             const now = new Date();
             setTime(now.toLocaleTimeString());
          }, 1000);
-         
-            
-         }, interval);
-        })
-        <h1>DIGITAL CLOCK</h1>
+
+         return () => clearInterval(interval);
+      }, []);
+
+        return 
+         <h1>DIGITAL CLOCK</h1>
      )
 }
